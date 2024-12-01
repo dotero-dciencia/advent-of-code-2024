f858569e04da2a2cf808b72f92276b509f7804ca
doterodev <doterodev@gmail.com>
2024-12-01 12:54:32 +0100

    Solución más óptima y comentarios

--- 01/ex1.cpp
+++ 01/ex1.cpp
@@ -2,9 +2,11 @@
 #include <queue>
 #include <cmath>
 
+// Solución en O(n)
+
 int main(){
-  
-  // Definición de variables: n1 -> número de la izquierda; n2 -> número de la derecha; solucion -> donde guardar la solución; lst1 -> todos los números de la izquierda; lst2 -> todos los números de la derecha.
+
+  // Definición de variables: n1 -> número de la izquierda; n2 -> número de la derecha; solucion -> donde guardar la solución; lst1 -> todos los números de la izquierda; lst2 -> todos los números de la derecha
   int n1, n2, solucion = 0;
   std::priority_queue<int> lst1, lst2;
 
@@ -14,13 +16,13 @@ int main(){
     lst1.push(n1); lst2.push(n2);
   }
 
-  // Mientras queden números, se restan los mayores de cada grupo y el valor absoluto se añade a la solución. También se eliminan.
+  // Mientras queden números, se restan los mayores de cada grupo y el valor absoluto se añade a la solución. También se eliminan
   while (!lst1.empty()){
     solucion += abs(lst1.top() - lst2.top());
     lst1.pop(); lst2.pop();
   }
 
-  // Fin del programa e imprimimos solución.
+  // Imprimimos solución y fin del programa
   std::cout << solucion;
   return 0;
 }
--- 01/ex2.cpp
+++ 01/ex2.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
 #include <queue>
 #include <cmath>
+#include <map>
+
+// Solución en O(n)
 
 int main(){
-  int n1, n2, contador, solucion = 0;
+
+  // Definición de variables: n1 -> número de la izquierda; n2 -> número de la derecha; solucion -> donde guardar la solución; diccionario -> pares de valores con la cantidad de veces que aparece cada número en la lst2; lst1 -> todos los números de la izquierda; lst2 -> todos los números de la derecha
+  int n1, n2, solucion = 0;
+  std::map<int, int> diccionario;
   std::vector<int> lst1, lst2;
+
+  // Lectura de entrada
   for (int i = 0; i < 1000; ++i){
     std::cin >> n1 >> n2;
     lst1.push_back(n1); lst2.push_back(n2);
   }
-  for (int i = 0; i < lst1.size(); ++i){
-    contador = 0;
-    for (int j = 0; j < lst2.size(); ++j){
-      if (lst1[i] == lst2[j]){
-        contador += 1;
-      }
+
+  // Añadir la cantidad de veces que aparece cada número al diccionario
+  for (int i = 0; i < lst2.size(); ++i){
+
+    // Si ese número no esta registrado, lo añadimos con 1 aparición. Si sí está registrado, le añadimos 1 aparición más
+    if (diccionario.find(lst2[i]) == diccionario.end()){
+      diccionario[lst2[i]] = 1;
+    } else {
+      diccionario[lst2[i]] += 1;
     }
-    solucion += contador*lst1[i];
   }
+
+  // Por cada número de la primera lista, vemos cuántas veces aparece en la segunda y sumamos a la solución el producto del número con las veces que aparece
+  for (int i = 0; i < lst1.size(); ++i){
+    solucion += lst1[i]*diccionario[lst1[i]];
+  }
+
+  // Imprimimos solución y fin del programa
   std::cout << solucion;
   return 0;
 }
